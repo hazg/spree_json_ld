08b2b8478f586809f1acd5e3a8add9cd4f9699a2
Aleksandr Aleksandrov <hazg@mail.ru>
2019-06-22 10:23:43 +0300

    up

--- app/helpers/spree/json_ld_helper.rb
+++ app/helpers/spree/json_ld_helper.rb
@@ -1,21 +1,21 @@
 module Spree
 	module JsonLdHelper
-		
-		
+
+
 
 		def json_ld_path_to_url(path)
 			@config = Spree::JsonLd::Config
 			@host = @config.get(:url).sub(%r[^http://],'').sub(%r[/$], '')
-			
+
 			#TODO: https support
 			"http://#{@host.sub(%r[^http://],'')}/#{path.sub(%r[^/],'')}"
     end
-    
+
 		def generate_json_ld(product)
 
 			json = {
 				'@context' => "http://schema.org/",
-				'@type' => "Product",	
+				'@type' => "Product",
 				'name' => product.name,
 				'image' => (product.images.empty? ? false : json_ld_path_to_url(product.images.first.attachment.url(:product, false))),
 				'description' => product.description,
@@ -54,11 +54,11 @@ module Spree
   		 	# "@type": "AggregateRating",
     		# "ratingValue": "4.4",
     		#	"reviewCount": "89"
-  
+
   		}
-			
+
 			json = modify_json_ld(product, json)	if defined? 'modify_json_ld'
-			
+
 			json
 		end
 	end
